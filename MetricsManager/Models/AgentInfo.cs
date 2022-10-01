a8d7a8e12f8be4d9f79a9015f354953da2ff65b8
Mirza <mirzano05@gmail.com>
2022-10-02 00:16:42 +0300

    0.1

---
+++ MetricsManager/Models/AgentInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MetricsManager.Models
+{
+    public class AgentInfo
+    {
+        public int AgentId { get; set; }
+        public Uri AgenAddress { get; set; }
+        public bool Enable { get; set; }
+    }
+}
---
+++ MetricsManagerTests/CpuMetricsControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using MetricsManager.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsManagerTests
+{
+    public class CpuMetricsControllerTests
+    {
+        private CpuMetricsController _cpuMetricsController;
+
+        public CpuMetricsControllerTests()
+        {
+            _cpuMetricsController = new CpuMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnOk()
+        {
+
+        }
+    }
+}
